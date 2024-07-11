b105f1757ee825c613b4e61680b0610c460b3496
SeongWook <9892yyyssww@gmail.com>
2024-07-11 15:43:26 +0900

    [Feat] PlayerController 스크립트 작성

--- Assets/02.Scripts/Player/MoveMent.cs
+++ Assets/02.Scripts/Player/MoveMent.cs
@@ -1,35 +1,28 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveMent : MonoBehaviour
 {
-    [Header("MoveSpeed")]
-    [SerializeField] private float walkSpeed;
+    [Header("MoveSpeed")] [SerializeField] private float walkSpeed;
 
-    [Header("ChangeSpeedValue")]
-    [SerializeField] private float changeSpeedValue;
+    [Header("ChangeSpeedValue")] [SerializeField]
+    private float changeSpeedValue;
 
     #region PlayerComponent
-    private Rigidbody rigidBody;
+
+    private Rigidbody _rigidBody;
 
     #endregion
 
     #region PlayerValue
-    //player TargetSpeed
-    private float targetSpeed;
-    //player Speed
-    private float mySpeed;
-    //speed OffSet
-    private float speedOffSet = 0.1f;
-    //rotationVelocity
-    private float rotationVelocity;
-    //click RayPosition
-    private Vector3 clickRayPosition;
+
+    private float _targetSpeed; // player TargetSpeed
+    private float _mySpeed; // player Speed
+    private float _speedOffSet = 0.1f; // speed OffSet
+    private float rotationVelocity; // rotationVelocity
+    private Vector3 clickRayPosition; // click RayPosition
+    private bool isMoving; // isMoving
+
     #endregion
-
-
 
 
     private void Start()
@@ -49,7 +42,7 @@ public class MoveMent : MonoBehaviour
 
     private void InitPlayer()
     {
-        rigidBody = GetComponent<Rigidbody>();  
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
     private void MouseClick()
@@ -63,46 +56,49 @@ public class MoveMent : MonoBehaviour
             if (Physics.Raycast(rayPosition, out RaycastHit hit, Mathf.Infinity))
             {
                 clickRayPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                isMoving = true;
             }
         }
     }
 
     private void PlayerMovement()
     {
-        targetSpeed = walkSpeed;
-
-        if(rigidBody.velocity == Vector3.zero)
+        if (isMoving)
         {
-            targetSpeed = 0;
+            Vector3 moveDirection = (clickRayPosition - transform.position).normalized;
+
+            if (Vector3.Distance(transform.position, clickRayPosition) < _speedOffSet)
+            {
+                _rigidBody.velocity = Vector3.zero;
+                isMoving = false;
+
+                return;
+            }
+
+            _targetSpeed = walkSpeed;
+
+            float currentVelocity = new Vector3(_rigidBody.velocity.x, 0f, _rigidBody.velocity.z).magnitude;
+
+            if (currentVelocity < _targetSpeed - _speedOffSet || currentVelocity > _targetSpeed + _speedOffSet)
+            {
+                _mySpeed = Mathf.Lerp(currentVelocity, _targetSpeed, changeSpeedValue * Time.fixedDeltaTime);
+
+                _mySpeed = Mathf.Round(_mySpeed * 1000f) / 1000f;
+            }
+            else
+                _mySpeed = _targetSpeed;
+
+            if (_rigidBody.velocity != Vector3.zero)
+            {
+                float targetRotation = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+
+                float smoothRotation =
+                    Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, 0.12f);
+
+                transform.rotation = Quaternion.Euler(0, smoothRotation, 0);
+            }
+
+            _rigidBody.velocity = moveDirection * _mySpeed;
         }
-
-        float currentVelocity = new Vector3(rigidBody.velocity.x, 0f, rigidBody.velocity.z).magnitude;
-
-        if (currentVelocity < targetSpeed - speedOffSet || currentVelocity > targetSpeed + speedOffSet)
-        {
-            mySpeed = Mathf.Lerp(currentVelocity, targetSpeed, changeSpeedValue * Time.fixedDeltaTime);
-
-            mySpeed = Mathf.Round(mySpeed * 1000f) / 1000f;
-        }
-        else
-            mySpeed = targetSpeed;
-
-        Vector3 rotateDirection = (clickRayPosition - transform.position).normalized;
-
-        if(rigidBody.velocity != Vector3.zero)
-        {
-            float targetRotation = Mathf.Atan2(rotateDirection.x, rotateDirection.z) * Mathf.Rad2Deg;
-
-            float smoothRotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, 0.12f);
-
-            transform.rotation = Quaternion.Euler(0, smoothRotation, 0);
-        }
-
-        Vector3 moveDirection = (clickRayPosition - transform.position).normalized;
-
-        rigidBody.velocity = moveDirection * mySpeed;
-
     }
-
-
 }
