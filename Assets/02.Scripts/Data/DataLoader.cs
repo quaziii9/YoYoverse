5ff1fdac667bf891909d41b224a7a1c0c941cf88
Seongwook <9892yyyssww@gmail.com>
2024-07-13 23:01:49 +0900

    [Feat] 장비 UI 스크롤뷰 제작
    DiskListScrollView, WireListScrollView 제작 중

--- Assets/02.Scripts/Data/DataLoader.cs
+++ Assets/02.Scripts/Data/DataLoader.cs
@@ -1,35 +1,33 @@
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 
+// 데이터를 로드하는 정적 클래스
 public static class DataLoader<T> where T : class
 {
     // JSON 파일에서 데이터를 로드합니다.
     public static List<T> LoadDataFromJson(string fileName)
     {
-#if UNITY_EDITOR
-        string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
-#else
-        string filePath = Path.Combine(Application.persistentDataPath, fileName);
-#endif
+        // Resources 폴더에서 JSON 파일을 로드
+        TextAsset jsonFile = Resources.Load<TextAsset>(fileName);
 
-        // 파일이 존재하면 데이터를 읽고, 존재하지 않으면 빈 리스트를 반환합니다.
-        if (File.Exists(filePath))
+        // 파일이 존재하면 데이터를 읽고, 존재하지 않으면 빈 리스트를 반환
+        if (jsonFile != null)
         {
-            string json = File.ReadAllText(filePath);
+            string json = jsonFile.text;
             DataWrapper<T> dataWrapper = JsonUtility.FromJson<DataWrapper<T>>(json);
             return dataWrapper.data;
         }
         else
         {
-            Debug.LogError("File not found: " + filePath);
+            Debug.LogError("File not found: " + fileName);
             return new List<T>();
         }
     }
 
+    // JSON 데이터를 담는 내부 클래스
     [System.Serializable]
     private class DataWrapper<TData>
     {
-        public List<TData> data; // 제네릭 데이터를 담는 리스트
+        public List<TData> data;
     }
 }
---
+++ Assets/02.Scripts/YoYo/WireListItem.cs
@@ -0,0 +1,26 @@
+using Gpm.Ui;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class WireListItem : InfiniteScrollItem, IPointerDownHandler
+{
+    public Image image; // 와이어 이미지
+
+    private bool _isSelected;
+
+    public override void UpdateData(InfiniteScrollData scrollData)
+    {
+        YoYoData yoyoData = scrollData as YoYoData;
+        if (yoyoData == null) return;
+
+        // 텍스트와 이미지 업데이트
+        image.sprite = Resources.Load<Sprite>(yoyoData.imagePath);
+    }
+    
+    // 터치 입력을 처리하는 메서드
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // 선택된 디스크, 와이어에 업데이트
+    }
+}
