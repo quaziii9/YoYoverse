4d79029cb728a542e67787797705416e8b24753a
junHyung <jin05003@naver.com>
2024-07-14 21:12:40 +0900

    [feat] Data
    데이터 작성중.

--- Assets/02.Scripts/GameData/GameData.cs
+++ Assets/02.Scripts/GameData/GameData.cs
@@ -1,12 +1,39 @@
 
-public class GameData
+public class _Player
 {
-    
+    public int _playerId { get; set; }
+    public string _playerName { get; set; }
+    public float _attackPower { get; set; }
+    public float _attackSpeed { get; set; }
+    public float _playerHealth { get; set; }    
+    public float _playerDefense { get; set; }   
+    public float _playerMoveSpeed { get; set; } 
+    public string _playerType { get; set; } 
+}
 
+public class _Enemy
+{
+    public int _enemyId { get; set; }
+    public string _enemyName { get; set; }
+    public float _attackPower { get; set; }
+    public float _attackCoolTime { get; set; }
+    public float _enemyHealth { get; set; }
+    public float _enemyDefense { get; set; }
+    public float _enemyMoveSpeed { get; set; }
+    public int _skillId { get; set; }
+}
 
+public enum Target
+{
+    Single,
+    Multi
+}
 
-
-
-
-
+public class _PlayerSkill
+{
+    public int _skillId { get; set; }
+    public string _skillName { get; set; }
+    public float _skillPower { get; set; }
+    public float _skillCoolTime { get; set; }
+   
 }
